9ffbd052ddffaef97bff1dad57fbfe634e28e25e
Love Zambre <zambrelove95@gmail.com>
2023-07-06 13:18:26 +0530

    Added other section

---
+++ Assets/index.js
@@ -0,0 +1,8 @@
+function OpenSideBar(){
+    var element = document.getElementById('SideBar');
+    element.classList.add('Active-SideBar');
+}
+function CloseSideBar(){
+    var element = document.getElementById('SideBar');
+    element.classList.remove('Active-SideBar');
+}
